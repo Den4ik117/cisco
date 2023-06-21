6612ffd84818d7a63055a5f4aa6c2e6f8eb5dc5e
Denis Zagvozdin <gaz91.91@mail.ru>
2023-06-21 18:46:36 +0500

    Инициализировал пустой Laravel-проект; установил необходимые для работы библиотеки; подготовил базовый шаблон страницы

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Noto Sans', ...defaultTheme.fontFamily.sans],
+            },
+        }
+    },
+    plugins: [],
+}
