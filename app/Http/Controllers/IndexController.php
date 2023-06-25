7d72bdc378774c8da690a33ada30072c85d92bf6
Denis <gaz91.91@mail.ru>
2023-06-25 16:45:20 +0500

    Добавил инфографики

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\TestType;
+use App\Models\Marathon;
+use App\Models\Test;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('index');
+        $stats = [
+            'marathon_resolved_tasks_count' => Marathon::query()->where('token_uuid', $request->cookie('guest'))->latest()->first()?->tasks()->whereNotNull('is_success')->count() ?? 0,
+            'marathon_tasks_count' => Marathon::query()->where('token_uuid', $request->cookie('guest'))->latest()->first()?->tasks()->count() ?? 0,
+            'exam_resolved_exercises_count' => Test::query()
+                    ->where('token_uuid', $request->cookie('guest'))
+                    ->where('type', TestType::Exam->value)
+                    ->latest()
+                    ->first()
+                    ?->exercises()
+                    ->whereNotNull('is_success')
+                    ->count() ?? 0,
+            'exam_exercises_count' => Test::query()
+                    ->where('token_uuid', $request->cookie('guest'))
+                    ->where('type', TestType::Exam->value)
+                    ->latest()
+                    ->first()
+                    ?->exercises()
+                    ->count() ?? 0,
+        ];
+
+        return view('index', compact(['stats']));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,8 +22,44 @@
             </a>
         </div>
 
-        <a class="bg-slate-800 hover:bg-slate-700 rounded px-4 py-2" href="{{ route('tests.index') }}">
+        <a class="bg-slate-800 group hover:bg-slate-700 rounded px-4 pt-2 pb-4" href="{{ route('tests.index') }}">
             <img class="block mx-auto" src="{{ Vite::asset('resources/images/Education.svg') }}" alt="Фото процесса обучения" width="240" height="240">
+
+            <span class="grid grid-cols-[min-content_min-content_min-content] justify-center gap-10 text-xs px-10">
+                <span class="flex flex-col gap-1 text-center">
+                    <span class="flex gap-0.5 justify-center">
+                        <span class="text-gray-300">{{ $stats['marathon_resolved_tasks_count'] }}</span>
+                        <span class="text-gray-500">/</span>
+                        <span class="text-gray-500">{{ $stats['marathon_tasks_count'] }}</span>
+                    </span>
+                    <span class="h-1 bg-gray-700 group-hover:bg-gray-600 relative w-16">
+                        <span class="absolute block h-1 bg-blue-500" style="width: {{ $stats['marathon_tasks_count'] === 0 ? 0 : ($stats['marathon_resolved_tasks_count'] / $stats['marathon_tasks_count'] * 100) }}%;"></span>
+                    </span>
+                    <span class="text-gray-300">Марафон</span>
+                </span>
+                <span class="flex flex-col gap-1 text-center">
+                    <span class="flex gap-0.5 justify-center">
+                        <span class="text-gray-300">{{ $stats['exam_resolved_exercises_count'] }}</span>
+                        <span class="text-gray-500">/</span>
+                        <span class="text-gray-500">{{ $stats['exam_exercises_count'] }}</span>
+                    </span>
+                    <span class="h-1 bg-gray-700 group-hover:bg-gray-600 relative w-16">
+                        <span class="absolute block h-1 bg-blue-500" style="width: {{ $stats['exam_exercises_count'] === 0 ? 0 : ($stats['exam_resolved_exercises_count'] / $stats['exam_exercises_count'] * 100) }}%;"></span>
+                    </span>
+                    <span class="text-gray-300">Экзамен</span>
+                </span>
+                <span class="flex flex-col gap-1 text-center">
+                    <span class="flex gap-0.5 justify-center">
+                        <span class="text-gray-300">0</span>
+                        <span class="text-gray-500">/</span>
+                        <span class="text-gray-500">0</span>
+                    </span>
+                    <span class="h-1 bg-gray-700 group-hover:bg-gray-600 relative w-16">
+                        <span class="absolute block h-1 bg-blue-500" style="width: 0;"></span>
+                    </span>
+                    <span class="text-gray-300">Модули</span>
+                </span>
+            </span>
         </a>
 
         <div class="grid grid-cols-2 gap-4">
