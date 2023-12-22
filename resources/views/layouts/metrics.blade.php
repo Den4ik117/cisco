4ef1559727db27d9c4e91810170be57797ffc030
Denis Zagvozdin <gaz91.91@mail.ru>
2023-12-23 01:35:07 +0500

    Обновляет метрики

--- resources/views/layouts/metrics.blade.php
+++ resources/views/layouts/metrics.blade.php
@@ -7,13 +7,13 @@
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(94082551, "init", {
+        ym(95969823, "init", {
             clickmap:true,
             trackLinks:true,
             accurateTrackBounce:true,
             webvisor:true
         });
     </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/94082551" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <noscript><div><img src="https://mc.yandex.ru/watch/95969823" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
 @endproduction
